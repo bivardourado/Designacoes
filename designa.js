69ba921363c86aac089bdbdd51f266b7628762c3
Bivar <bivardourado@gmail.com>
2023-02-20 20:29:05 -0300

    atualização da 20:20

--- designa.js
+++ designa.js
@@ -1,50 +1,90 @@
-const pessoas = [];
+// Definir as listas de homens e mulheres
+let homens = [];
+let mulheres = [];
 
-function adicionarPessoa() {
-    const nome = document.getElementById("nome").value;
-    const genero = document.getElementById("genero").value;
-    const nivel = document.getElementById("nivel").value;
+// Permitir que o usuário insira os nomes nas listas
+document.getElementById("homens").addEventListener("submit", (event) => {
+    event.preventDefault();
+    homens.push(event.target.elements.nome.value);
+    event.target.elements.nome.value = "";
+});
 
-    const pessoa = { nome, genero, nivel };
-    pessoas.push(pessoa);
+document.getElementById("mulheres").addEventListener("submit", (event) => {
+    event.preventDefault();
+    mulheres.push(event.target.elements.nome.value);
+    event.target.elements.nome.value = "";
+});
 
-    document.getElementById("nome").value = "";
+// Definir a matriz de níveis de habilidade
+const niveis = [
+    [4, 3],
+    [4, 2],
+    [3, 2],
+    [3, 1],
+    [2, 1],
+];
+
+// Função para selecionar uma pessoa aleatória de uma lista, que atenda às restrições
+function selecionarPessoa(lista, semanaAtual, semanaAnterior, sexo, nivelMax) {
+    let candidatos = lista.filter((pessoa) => {
+        return (
+            semanaAtual.indexOf(pessoa) === -1 &&
+            semanaAnterior.indexOf(pessoa) === -1 &&
+            (sexo === "M" ? homens : mulheres).indexOf(pessoa) !== -1
+        );
+    });
+
+    let selecionados = candidatos.filter((pessoa) => {
+        return niveis.indexOf([nivelMax, pessoa.nivel]) !== -1;
+    });
+
+    if (selecionados.length > 0) {
+        return selecionados[Math.floor(Math.random() * selecionados.length)];
+    } else if (candidatos.length > 0) {
+        return candidatos[Math.floor(Math.random() * candidatos.length)];
+    } else {
+        return null;
+    }
 }
 
-function gerarPares() {
-    const numeroPares = document.getElementById("numeroPares").value;
-    const paresGerados = document.getElementById("paresGerados");
-    paresGerados.innerHTML = "";
+// Função para gerar um par a partir de duas pessoas selecionadas aleatoriamente
+function gerarPar(semanaAtual, semanaAnterior, nivelMax) {
+    let homemPrincipal = selecionarPessoa(homens, semanaAtual, semanaAnterior, "M", nivelMax);
+    let homemSecundario = selecionarPessoa(homens, semanaAtual, semanaAnterior, "M", nivelMax, [homemPrincipal]);
+    let mulherPrincipal = selecionarPessoa(mulheres, semanaAtual, semanaAnterior, "F", nivelMax);
+    let mulherSecundaria = selecionarPessoa(mulheres, semanaAtual, semanaAnterior, "F", nivelMax, [mulherPrincipal]);
 
-    if (numeroPares < 1 || numeroPares > pessoas.length / 2) {
-        paresGerados.innerHTML = "Número de pares inválido";
-        return;
-    }
+    return {
+        homemPrincipal,
+        homemSecundario,
+        mulherPrincipal,
+        mulherSecundaria
+    };
+}
 
-    // Filtrar a lista de pessoas por gênero
-    const homens = pessoas.filter(pessoa => pessoa.genero === "Homem");
-    const mulheres = pessoas.filter(pessoa => pessoa.genero === "Mulher");
+function gerarPares(listaNomes) {
+    const homens = listaNomes.homens;
+    const mulheres = listaNomes.mulheres;
 
-    // Embaralhar as listas de homens e mulheres separadamente
-    shuffleArray(homens);
-    shuffleArray(mulheres);
+    // ordena as listas por nível, do maior para o menor
+    const homensOrdenados = homens.sort((a, b) => b.nivel - a.nivel);
+    const mulheresOrdenadas = mulheres.sort((a, b) => b.nivel - a.nivel);
 
-    // Juntar as listas embaralhadas em uma única lista com gêneros alternados
-    const pessoasEmbaralhadas = [];
-    for (let i = 0; i < numeroPares; i++) {
-        pessoasEmbaralhadas.push(homens[i], mulheres[i]);
-    }
-
-    // Dividir a lista de pessoas em pares
     const pares = [];
-    for (let i = 0; i < numeroPares; i++) {
-        pares.push([pessoasEmbaralhadas[i * 2], pessoasEmbaralhadas[i * 2 + 1]]);
+
+    // gera os pares aleatoriamente
+    while (homensOrdenados.length > 0 && mulheresOrdenadas.length > 0) {
+        const indexHomem = Math.floor(Math.random() * homensOrdenados.length);
+        const indexMulher = Math.floor(Math.random() * mulheresOrdenadas.length);
+        const homem = homensOrdenados.splice(indexHomem, 1)[0];
+        const mulher = mulheresOrdenadas.splice(indexMulher, 1)[0];
+        pares.push({
+            homemPrincipal: homem,
+            homemSecundario: homensOrdenados.shift(),
+            mulherPrincipal: mulher,
+            mulherSecundaria: mulheresOrdenadas.shift(),
+        });
     }
 
-    // Exibir os pares gerados na tela
-    pares.forEach(par => {
-        const li = document.createElement("li");
-        li.textContent = `${par[0].nome} (${par[0].genero}, ${par[0].nivel}) e ${par[1].nome} (${par[1].genero}, ${par[1].nivel})`;
-        paresGerados.appendChild(li);
-    });
+    return pares;
 }
