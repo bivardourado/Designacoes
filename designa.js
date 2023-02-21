88dbac255d4756840d40dc04c6caabad4122933e
Bivar <bivardourado@gmail.com>
2023-02-20 21:11:04 -0300

    Atualização das 21:00

--- designa.js
+++ designa.js
@@ -1,46 +1,30 @@
-// Definir as listas de homens e mulheres
-let homens = [];
-let mulheres = [];
-
-// Permitir que o usuário insira os nomes nas listas
-document.getElementById("homens").addEventListener("submit", (event) => {
-    event.preventDefault();
-    homens.push(event.target.elements.nome.value);
-    event.target.elements.nome.value = "";
-});
-
-document.getElementById("mulheres").addEventListener("submit", (event) => {
-    event.preventDefault();
-    mulheres.push(event.target.elements.nome.value);
-    event.target.elements.nome.value = "";
-});
-
-// Definir a matriz de níveis de habilidade
-const niveis = [
-    [4, 3],
-    [4, 2],
-    [3, 2],
-    [3, 1],
-    [2, 1],
+// Definir as listas de homens e mulheres com seus níveis de habilidade
+let homens = [
+    { nome: "Homem 1", nivel: "iniciante" },
+    { nome: "Homem 2", nivel: "intermediário" },
+    { nome: "Homem 3", nivel: "avançado" },
+    { nome: "Homem 4", nivel: "intermediário" },
+    { nome: "Homem 5", nivel: "iniciante" },
+];
+let mulheres = [
+    { nome: "Mulher 1", nivel: "iniciante" },
+    { nome: "Mulher 2", nivel: "intermediário" },
+    { nome: "Mulher 3", nivel: "avançado" },
+    { nome: "Mulher 4", nivel: "avançado" },
+    { nome: "Mulher 5", nivel: "intermediário" },
 ];
 
 // Função para selecionar uma pessoa aleatória de uma lista, que atenda às restrições
-function selecionarPessoa(lista, semanaAtual, semanaAnterior, sexo, nivelMax) {
+function selecionarPessoa(lista, semanaAtual, semanaAnterior, nivelMax) {
     let candidatos = lista.filter((pessoa) => {
         return (
             semanaAtual.indexOf(pessoa) === -1 &&
             semanaAnterior.indexOf(pessoa) === -1 &&
-            (sexo === "M" ? homens : mulheres).indexOf(pessoa) !== -1
+            pessoa.nivel === nivelMax
         );
     });
 
-    let selecionados = candidatos.filter((pessoa) => {
-        return niveis.indexOf([nivelMax, pessoa.nivel]) !== -1;
-    });
-
-    if (selecionados.length > 0) {
-        return selecionados[Math.floor(Math.random() * selecionados.length)];
-    } else if (candidatos.length > 0) {
+    if (candidatos.length > 0) {
         return candidatos[Math.floor(Math.random() * candidatos.length)];
     } else {
         return null;
@@ -49,41 +33,36 @@ function selecionarPessoa(lista, semanaAtual, semanaAnterior, sexo, nivelMax) {
 
 // Função para gerar um par a partir de duas pessoas selecionadas aleatoriamente
 function gerarPar(semanaAtual, semanaAnterior, nivelMax) {
-    let homemPrincipal = selecionarPessoa(homens, semanaAtual, semanaAnterior, "M", nivelMax);
-    let homemSecundario = selecionarPessoa(homens, semanaAtual, semanaAnterior, "M", nivelMax, [homemPrincipal]);
-    let mulherPrincipal = selecionarPessoa(mulheres, semanaAtual, semanaAnterior, "F", nivelMax);
-    let mulherSecundaria = selecionarPessoa(mulheres, semanaAtual, semanaAnterior, "F", nivelMax, [mulherPrincipal]);
+    let pessoaPrincipal = selecionarPessoa(homens, semanaAtual, semanaAnterior, nivelMax);
+    let pessoaSecundaria = selecionarPessoa(homens, semanaAtual, semanaAnterior, nivelMax);
+    if (!pessoaPrincipal || !pessoaSecundaria) {
+        pessoaPrincipal = selecionarPessoa(mulheres, semanaAtual, semanaAnterior, nivelMax);
+        pessoaSecundaria = selecionarPessoa(mulheres, semanaAtual, semanaAnterior, nivelMax);
+    }
 
     return {
-        homemPrincipal,
-        homemSecundario,
-        mulherPrincipal,
-        mulherSecundaria
+        pessoaPrincipal,
+        pessoaSecundaria
     };
 }
 
 function gerarPares(listaNomes) {
     const homens = listaNomes.homens;
     const mulheres = listaNomes.mulheres;
-
-    // ordena as listas por nível, do maior para o menor
-    const homensOrdenados = homens.sort((a, b) => b.nivel - a.nivel);
-    const mulheresOrdenadas = mulheres.sort((a, b) => b.nivel - a.nivel);
-
     const pares = [];
 
-    // gera os pares aleatoriamente
-    while (homensOrdenados.length > 0 && mulheresOrdenadas.length > 0) {
-        const indexHomem = Math.floor(Math.random() * homensOrdenados.length);
-        const indexMulher = Math.floor(Math.random() * mulheresOrdenadas.length);
-        const homem = homensOrdenados.splice(indexHomem, 1)[0];
-        const mulher = mulheresOrdenadas.splice(indexMulher, 1)[0];
-        pares.push({
-            homemPrincipal: homem,
-            homemSecundario: homensOrdenados.shift(),
-            mulherPrincipal: mulher,
-            mulherSecundaria: mulheresOrdenadas.shift(),
-        });
+    // seleciona os níveis existentes na lista de homens e mulheres
+    const niveis = [...new Set([...homens, ...mulheres].map((pessoa) => pessoa.nivel))];
+
+    // gera um par para cada nível
+    for (let nivel of niveis) {
+        const semanaAtual = pares.flatMap((par) => [par.pessoaPrincipal, par.pessoaSecundaria]);
+        const semanaAnterior = homens.concat(mulheres).filter((pessoa) => semanaAtual.indexOf(pessoa) === -1);
+
+        const par = gerarPar(semanaAtual, semanaAnterior, nivel);
+        if (par) {
+            pares.push(par);
+        }
     }
 
     return pares;
